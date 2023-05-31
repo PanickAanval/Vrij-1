9d35c4f2fc3bf4765694b89c507580b0b895a3d8
Doedeledoedel <84157343+Diedeliedee@users.noreply.github.com>
2023-06-01 00:25:22 +0200

    Ascended the camera script to be smoother :))

--- Monster Game!!/Assets/Objects/Player/Camera/Camera.cs
+++ Monster Game!!/Assets/Objects/Player/Camera/Camera.cs
@@ -15,49 +15,51 @@ public class Camera : MonoBehaviour
     [SerializeField] private float m_referenceSpeed = 3f;
     [SerializeField] private float m_adjustmentTime = 3f;
 
-    private CameraRecords m_records = null;
+    //  Orbiting data:
     private Orbit m_orbit = new Orbit();
+    private Vector3 m_orbitCenter = Vector3.zero;
 
+    //  Tracking the player:
     private Vector3 m_followVelocity = Vector3.zero;
 
+    //  Adjusting the look direction:
     private Vector2 m_desiredLookDir = Vector2.zero;
     private float m_adjustmentVelocity = 0f;
 
+    //  References:
     private Transform m_target = null;
 
     public void Setup(Transform target)
     {
-        m_records = new CameraRecords(transform, target);
-        m_orbit.SetOffset(m_records.offset);
+        var offsetFromTarget = transform.position - target.position;
 
+        m_orbitCenter = target.position;
+        m_orbit.SetOffset(offsetFromTarget);
         m_target = target;
 
-        SetDesiredDir(Vectors.VectorToFlat(m_records.offset));
+        SetDesiredDir(Vectors.VectorToFlat(offsetFromTarget));
     }
 
     public void Tick(Vector2 input, Vector2 playerVel, float deltaTime)
     {
         transform.position = GetDesiredPosition(input, playerVel, deltaTime);
         transform.rotation = Quaternion.LookRotation(m_target.position - transform.position, Vector3.up);
-        m_records.Update(transform, m_target);
     }
 
     /// <returns>The desired position of the camera.</returns>
     private Vector3 GetDesiredPosition(Vector2 input, Vector2 playerVel, float deltaTime)
     {
-        var currentPos = transform.position;
-        var desiredPos = m_target.position + GetDesiredOffset(input, playerVel, deltaTime);
-
-        currentPos = Vector3.SmoothDamp(currentPos, desiredPos, ref m_followVelocity, m_followTime);
-        return desiredPos;
+        m_orbitCenter = Vector3.SmoothDamp(m_orbitCenter, m_target.position, ref m_followVelocity, m_followTime);
+        return m_orbitCenter + GetDesiredOffset(input, playerVel, deltaTime);
     }
 
     /// <returns>The desired offset from the player.</returns>
     private Vector3 GetDesiredOffset(Vector2 input, Vector2 playerVel, float deltaTime)
     {
+        //  Save the player's last recorded horizontal velocity.
         if (playerVel != Vector2.zero)
         {
-            SetDesiredDir(-playerVel);                  //  Save the player's last recorded horizontal velocity.
+            SetDesiredDir(-playerVel);
         }
 
         var yAngle = GetYAngle(input.x);
@@ -111,31 +113,6 @@ public class Camera : MonoBehaviour
     {
         if (!Application.isPlaying) return;
         if (m_target == null) return;
-        GizmoTools.DrawSphere(m_target.position, m_orbit.distance, Color.white, 0.5f, true, 0.1f);
-    }
-
-    /// <summary>
-    /// Class containing information about the camera the previous frame.
-    /// </summary>
-    private class CameraRecords
-    {
-        public Vector3 position;
-        public Vector3 offset;
-        public Vector3 upDirection;
-        public Vector3 rightDirection;
-        public Vector3 playerVelocity;
-
-        public CameraRecords(Transform camera, Transform target)
-        {
-            Update(camera, target);
-        }
-
-        public void Update(Transform camera, Transform target)
-        {
-            position = camera.position;
-            offset = camera.position - target.position;
-            upDirection = camera.up;
-            rightDirection = camera.right;
-        }
+        GizmoTools.DrawSphere(m_orbitCenter, m_orbit.distance, Color.white, 0.5f, true, 0.1f);
     }
 }
