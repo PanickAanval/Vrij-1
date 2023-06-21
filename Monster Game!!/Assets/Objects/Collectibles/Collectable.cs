564028c663640a0515f387f06e3a2e1fb84e9f54
Nick Hordijk <yourhomieniki@gmail.com>
2023-06-21 22:40:53 +0200

    de rest

--- Monster Game!!/Assets/Objects/Collectibles/Collectable.cs
+++ Monster Game!!/Assets/Objects/Collectibles/Collectable.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Events;
 
 public class Collectable : MonoBehaviour
 {
     public TMP_Text MyText;
     public int amountScrews = 0;
     public int amountCogs = 0;
+    public UnityEvent Genoeg;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,10 @@ public class Collectable : MonoBehaviour
     void Update()
     {
         MyText.text = "Schroeven:" + amountScrews;
+        if (amountScrews == 10)
+        {
+            Genoeg.Invoke();
+        }
     }
 
     public void IncreaseCounterScrew()
