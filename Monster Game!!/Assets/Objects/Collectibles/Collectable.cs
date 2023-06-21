3a52e9b86d89327856f27e5d161886fe3c56a32f
Nick Hordijk <yourhomieniki@gmail.com>
2023-06-21 22:27:39 +0200

    COLLECTIBLE COUNTER #SLAY!!!!!!!

--- Monster Game!!/Assets/Objects/Collectibles/Collectable.cs
+++ Monster Game!!/Assets/Objects/Collectibles/Collectable.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+using TMPro;
+
 public class Collectable : MonoBehaviour
 {
+    public TMP_Text MyText;
     public int amountScrews = 0;
     public int amountCogs = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        MyText.text = "Schroeven:";
     }
     // Update is called once per frame
     void Update()
     {
+        MyText.text = "Schroeven:" + amountScrews;
     }
 
     public void IncreaseCounterScrew()
