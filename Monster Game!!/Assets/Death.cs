b7dbc1db3ed20a74c1ea2aa0a2e34363d52fb782
Rick-van-Leeuwen <124429419+Rick-van-Leeuwen@users.noreply.github.com>
2023-06-22 09:19:08 +0200

    Death now enabled
    You can die

---
+++ Monster Game!!/Assets/Death.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Player player))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
